2ac4f17942415cc8ac0c3f04046582896482ca85
Your Name <prateekpriyesh@gmail.com>
2020-09-03 07:52:23 +0530

    minipro1

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<>
+<h1>List Of Fav Movies</h1>
+<ol>
+  <li>Movie1</li>
+  <li>Movie 2</li>
+  <li>Movie 3</li>
+  <li>Movie 4</li>
+  <li>Movie 5</li>
+</ol>
+ </>
+  ,document.getElementById('root'));
